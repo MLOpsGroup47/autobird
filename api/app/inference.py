2d7ba02cf014887400975584bbb327f0961faa05
U-GUSTAVLAPTOP\gusta <https://justavgunker@github.com>
2026-01-21 17:34:22 +0100

    formatting fixes

--- api/app/inference.py
+++ api/app/inference.py
@@ -2,19 +2,18 @@ from contextlib import asynccontextmanager
 from tkinter import Image
 
 import hydra
-from call_of_func.dataclasses.pathing import PathConfig
-from call_of_func.utils.get_source_path import ROOT, CONFIG_DIR
 import torch
-from fastapi import FastAPI, File, UploadFile
-from call_of_func.data.processing import preprocess_cfg
 from call_of_birds_autobird.model import Model
+from call_of_func.data.processing import preprocess_cfg
+from call_of_func.dataclasses.pathing import PathConfig
+from call_of_func.utils.get_source_path import CONFIG_DIR, ROOT
+from fastapi import FastAPI, File, UploadFile
 from omegaconf import DictConfig
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Load and clean up model on startup and shutdown."""
-    global model, feature_extractor, tokenizer, device, gen_kwargs
     print("Loading model")
     model = Model()
     model.load_state_dict(torch.load(PathConfig.ckpt_dir / "last.pth", map_location="cpu"))
@@ -26,8 +25,6 @@ async def lifespan(app: FastAPI):
 
     yield
 
-    print("Cleaning up")
-    del model, feature_extractor, tokenizer, device, gen_kwargs
 
 
 app = FastAPI(lifespan=lifespan)
@@ -35,18 +32,11 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/classify/")
 async def caption(data: UploadFile = File(...)):
+    pass
 
-@hydra.main(version_base=None, config_path=CONFIG_DIR, config_name="configs")
-def predict_step(cfg:DictConfig, filepath: str) -> str:
-    print("Loading model")
-    model = Model()
-    model.load_state_dict(torch.load(PathConfig.ckpt_dir / "last.pth", map_location="cpu"))
 
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+def predict_step(filepath: str) -> str:
 
-    pr
 
     pred = "bird"
     
