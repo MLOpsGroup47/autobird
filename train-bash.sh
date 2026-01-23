f7d18eeb79ed11e4dc61617bb5b6881e21f684ed
MortenKleist <s224022@dtu.dk>
2026-01-23 10:49:33 +0100

    Wandb logging through cloud

--- train-bash.sh
+++ train-bash.sh
@@ -3,7 +3,7 @@
 
 gcloud ai custom-jobs create \
   --region=europe-west1 \
-  --display-name=test-run \
+  --display-name=test-run-parallel \
   --config=config_gpu.yaml \
   --args="paths.processed_dir=gs://birdcage-bucket/data/processed" \
   --args="paths.x_train=gs://birdcage-bucket/data/processed/train_x.pt" \
